c0ed1a685d782bc634444501e649ef61efdf6693
Markus Heiser <markus.heiser@darmarit.de>
2022-09-22 18:45:15 +0200

    [mod] ddg weather: switch to data types from searx.result.weather (WIP)
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/engines/duckduckgo_weather.py
+++ searx/engines/duckduckgo_weather.py
@@ -31,6 +31,84 @@ def f_to_c(temperature):
     return "%.2f" % ((temperature - 32) / 1.8)
 
 
+def map_ddw_weather_data(w_item: dict):
+
+    # WeatherPeriodType
+
+    w_period = {
+        'air_temperature_max': {'F': w_item.get('temperatureHigh')},
+        'air_temperature_min': {'F': w_item.get('temperatureLow')},
+        'precipitation_amount': w_item.get('precipIntensity'),
+        'precipitation_amount_max': w_item.get('precipIntensityMax'),
+        'probability_of_precipitation': w_item.get('precipIntensity'),
+        'probability_of_thunder': None,
+        'ultraviolet_index_clear_sky_max': w_item.get('uvIndex'),
+    }
+
+    # normalize the values
+
+    if w_period['air_temperature_max']['F'] is not None:
+        w_period['air_temperature_max']['C'] = f_to_c(w_period['air_temperature_max']['F'])
+
+    if w_period['air_temperature_min']['F'] is not None:
+        w_period['air_temperature_min']['C'] = f_to_c(w_period['air_temperature_min']['F'])
+
+    if w_period['precipitation_amount'] is not None:
+        w_period['precipitation_amount'] = w_period['precipitation_amount'] * 1000
+
+    if w_period['precipitation_amount_max'] is not None:
+        w_period['precipitation_amount_max'] = w_period['precipitation_amount_max'] * 1000
+
+    if w_period['precipIntensity'] is not None:
+        w_period['precipIntensity'] = w_period['precipIntensity'] * 100
+
+    # WeatherInstantType
+
+    w_instant = {
+        'air_pressure_at_sea_level': w_item.get('pressure'),
+        'air_temperature': w_item.get('temperature'),
+        'air_temperature_percentile_10': None,
+        'air_temperature_percentile_90': None,
+        'cloud_area_fraction': w_item.get('cloudCover'),
+        'cloud_area_fraction_high': None,
+        'cloud_area_fraction_low': None,
+        'cloud_area_fraction_medium': None,
+        'dew_point_temperature': {'F': w_item.get('dewPoint')},
+        'fog_area_fraction': None,
+        'relative_humidity': w_item.get('humidity'),
+        'wind_from_direction': w_item.get('windBearing'),
+        'wind_speed': w_item.get('windSpeed'),
+        'wind_speed_of_gust': w_item.get('windGust'),
+        'wind_speed_percentile_10': None,
+        'wind_speed_percentile_90': None,
+    }
+
+    # normalize the values
+
+    if w_instant['cloud_area_fraction'] is not None:
+        w_instant['cloud_area_fraction'] = w_instant['cloud_area_fraction'] * 100
+
+    if w_instant['dew_point_temperature']['F'] is not None:
+        w_instant['dew_point_temperature']['C'] = f_to_c(w_instant['dew_point_temperature']['F'])
+
+    if w_instant['relative_humidity'] is not None:
+        w_instant['relative_humidity'] = w_instant['relative_humidity'] * 100
+
+    # WeatherSummaryType
+
+    # TODO ...
+    w_summary = {}
+
+    # "icon": "clear-night",
+    # "icon": "rain",
+    # "icon": "clear-night",
+    # "icon": "partly-cloudy-night",
+    # "icon": "partly-cloudy-day",
+    # cloudy
+
+    return w_period, w_instant, w_summary
+
+
 def response(resp):
     results = []
 
@@ -38,6 +116,11 @@ def response(resp):
         return []
 
     result = loads(resp.text[resp.text.find('{') : resp.text.rfind('}') + 1])
+
+    # from json import dump
+    # with open("ddgw.json", "w") as f:
+    #     logger.debug("dump ddgw.json ...")
+    #     dump(result, f, indent=2)
 
     current = result["currently"]
 
